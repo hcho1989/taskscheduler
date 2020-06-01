41d673e682c31da3646f3c27d4124ae393507c9d
Ho Chi Ho <hochi.ho@scmp.com>
2020-06-01 17:34:31 +0800

    remove patter hour support, added day support

--- schedule/schedule.go
+++ schedule/schedule.go
@@ -3,6 +3,8 @@ package schedule
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/hcho1989/taskscheduler/pattern"
@@ -19,7 +21,7 @@ type Schedule struct {
 	StartFrom     time.Time
 	EndAt         time.Time
 	Pattern       pattern.PatternInterface
-	Instances     []string // Duration from beginning of period
+	Offset        *string // Duration from beginning of period, only support format "%sd", d stands for days
 	beforeExecute func(a string, b, c time.Time) (bool, error)
 	afterExecute  func(string, bool)
 }
@@ -28,42 +30,40 @@ var TIMEZONE, _ = time.LoadLocation("Asia/Hong_Kong")
 
 func (s Schedule) Execute(planName string, t task.TaskInterface, currentTime time.Time) {
 
-	for i, offset := range s.Instances {
-		if currentTime.After(s.EndAt) {
-			fmt.Println("Time now is after schedule.EndAt, skipped.", s.EndAt, currentTime)
-			continue
-		}
-		if currentTime.Before(s.StartFrom) {
-			fmt.Println("Time now is before schedule.StartFrom, skipped.", s.StartFrom, currentTime)
-			continue
-		}
-		success := false
-		fmt.Printf("Checking Plan %v offset %d\n", planName, i)
+	if currentTime.After(s.EndAt) {
+		fmt.Println("Time now is after schedule.EndAt, skipped.", s.EndAt, currentTime)
+		continue
+	}
+	if currentTime.Before(s.StartFrom) {
+		fmt.Println("Time now is before schedule.StartFrom, skipped.", s.StartFrom, currentTime)
+		continue
+	}
+	success := false
+	fmt.Printf("Checking Plan %v offset %d\n", planName, s.Offset)
 
-		if s.Pattern.IsBeyondPattern(currentTime) {
-			fmt.Println("current time lies beyond the defined pattern, skipped.")
-			continue
-		}
-		instance := s.Pattern.ResolveInstance(currentTime)
-		offsetDur, err := time.ParseDuration(offset)
+	if s.Pattern.IsBeyondPattern(currentTime) {
+		fmt.Println("current time lies beyond the defined pattern, skipped.")
+		continue
+	}
+	instance := s.Pattern.ResolveInstance(currentTime)
+	offsetDur, err = calulateDuration(s.Offset)
+	if err != nil {
+		fmt.Printf("Fail to parse start offset %s, skipped, error: %s\n", offset, err.Error())
+		continue
+	}
+	scheduleTime := instance.Add(offsetDur)
+
+	ok, err := s.beforeExecute(planName, scheduleTime, currentTime)
+	if ok {
+		fmt.Printf("Running Task: %s\n", planName)
+		success, err = t.Execute(scheduleTime)
 		if err != nil {
-			fmt.Printf("Fail to parse start offset %s, skipped, error: %s\n", offset, err.Error())
-			continue
+			fmt.Printf("Error when executing task, error: %s\n", err.Error())
 		}
-		scheduleTime := instance.Add(offsetDur)
-
-		ok, err := s.beforeExecute(planName, scheduleTime, currentTime)
-		if ok {
-			fmt.Printf("Running Task: %s\n", planName)
-			success, err = t.Execute(scheduleTime)
-			if err != nil {
-				fmt.Printf("Error when executing task, error: %s\n", err.Error())
-			}
-			fmt.Printf("%s Finished, success: %v\n", planName, success)
-			s.afterExecute(planName, success)
-		} else {
-			fmt.Println("Skipped")
-		}
+		fmt.Printf("%s Finished, success: %v\n", planName, success)
+		s.afterExecute(planName, success)
+	} else {
+		fmt.Println("Skipped")
 	}
 }
 
@@ -170,5 +170,24 @@ type patternJSON struct {
 		Start    string
 		End      string
 		Duration string
+	}
+}
+
+func calulateDuration(a *string) (*time.Duration, error) {
+	if a == nil {
+		r, _ := time.ParseDuration("0h")
+		return &r, nil
+	}
+	if len(a) > 0 && strings.Index(a, "d") == len(a)-1 {
+		// fmt.Println(int(a[:len(a)-1]))
+		i, err := strconv.ParseInt(a, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		r, err := time.ParseDuration(fmt.Printf("%sh", string(i*24)))
+		if err != nil {
+			return nil, err
+		}
+		return &r, nil
 	}
 }
